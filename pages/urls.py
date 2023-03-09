a457590f177a9152ebed0dd54ed54a35fc2885ae
Chinna Babu Korlagunta <119917273+chinnababu99@users.noreply.github.com>
2023-03-09 12:16:49 +0530

    first commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
 
-from .views import *
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
 
-urlpatterns=[
-    path('home/',home,name='home')
 ]
